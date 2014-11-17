f827d6eb4bb347f351c847385ccdfb3e3dd0a352
Matthew Pherigo <hybrid120@gmail.com>
2014-11-17 09:53:02 -0600

    get playbooks mostly functional

--- one-time-setup.sh
+++ one-time-setup.sh
@@ -4,7 +4,7 @@ echo $@ $#
 (($# == 2) || exit)
 
 #ssh -t ${1}@${2} 'env ASSUME_ALWAYS_YES=YES pkg bootstrap; pkg update; pkg install python'
-echo "$2" > tmphosts
+echo "$2 ansible_ssh_user=$1 ansible_python_interpreter=/usr/local/bin/python" > tmphosts
 ansible-playbook one-time-setup.yml -ki ./tmphosts
 rm tmphosts
 
