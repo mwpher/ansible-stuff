20002d529dc225efd63ddf6a044e65e97ac70f8e
Matthew Pherigo <hybrid120@gmail.com>
2014-11-28 18:51:32 -0600

    make python bootstrap automatic

--- one-time-setup.sh
+++ one-time-setup.sh
@@ -3,7 +3,7 @@ echo $@ $#
 
 (($# == 2) || exit)
 
-ssh -t ${1}@${2} 'env ASSUME_ALWAYS_YES=YES pkg bootstrap; pkg update; pkg install python'
+ssh -t ${1}@${2} 'env ASSUME_ALWAYS_YES=YES pkg bootstrap; env ASSUME_ALWAYS_YES=YES pkg update; env ASSUME_ALWAYS_YES=YES pkg install python'
 echo "$2 ansible_ssh_user=$1 ansible_python_interpreter=/usr/local/bin/python" > tmphosts
 ansible-playbook one-time-setup.yml -ki ./tmphosts
 rm tmphosts
