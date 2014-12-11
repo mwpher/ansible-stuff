1dba3d215e6b5cc74573cf039df87c554032a32f
Matthew Pherigo <hybrid120@gmail.com>
2014-12-11 15:05:15 -0600

    add the ability to bootstrap jails

--- one-time-setup.sh
+++ one-time-setup.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
+
+PYTHON_COMMAND='env ASSUME_ALWAYS_YES=YES pkg update; env ASSUME_ALWAYS_YES=YES pkg install python'
 echo $@ $#
 
-(($# == 2) || exit)
+([ $# == '2' ] || exit)
 
-ssh -t ${1}@${2} 'env ASSUME_ALWAYS_YES=YES pkg bootstrap; env ASSUME_ALWAYS_YES=YES pkg update; env ASSUME_ALWAYS_YES=YES pkg install python'
-echo "$2 ansible_ssh_user=$1 ansible_python_interpreter=/usr/local/bin/python" > tmphosts
-ansible-playbook one-time-setup.yml -ki ./tmphosts
+if [ $1 == '-j' ]; then
+	sudo ezjail-admin start $2
+	export ASSUME_ALWAYS_YES=YES
+	sudo pkg -j $2 update;
+	sudo pkg -j $2 install python;
+	echo "$2 ansible_connection=jail ansible_python_interpreter=/usr/local/bin/python" > tmphosts
+	sudo ansible-playbook one-time-setup.yml -i ./tmphosts
+else
+	ssh -t ${1}@${2} $PYTHON_COMMAND
+	echo "$2 ansible_connection=paramiko ansible_ssh_user=$1 ansible_python_interpreter=/usr/local/bin/python" > tmphosts
+	ansible-playbook one-time-setup.yml -ki ./tmphosts
+fi
 rm tmphosts
