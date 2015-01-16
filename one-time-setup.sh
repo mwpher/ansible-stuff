c0f91b8aa72604bf3c1cbdabdefb9e722a94e973
Matthew Pherigo <hybrid120@gmail.com>
2015-01-15 19:18:26 -0600

    fix one-time-setup.sh

--- one-time-setup.sh
+++ one-time-setup.sh
@@ -11,8 +11,12 @@ if [ $1 == '-j' ]; then
 	echo "$2 ansible_connection=jail ansible_python_interpreter=/usr/local/bin/python" > tmphosts
 	sudo ansible-playbook one-time-setup.yml -i ./tmphosts
 else
-	ssh -t ${1}@${2} $PYTHON_COMMAND
-	echo "$2 ansible_connection=paramiko ansible_ssh_user=$1 ansible_python_interpreter=/usr/local/bin/python" > tmphosts
+	if [ $1 == '-s' ]; then
+		echo "$3 ansible_connection=paramiko ansible_ssh_user=$2 ansible_python_interpreter=/usr/local/bin/python" > tmphosts
+	else
+		ssh -t ${1}@${2} $PYTHON_COMMAND
+		echo "$1 ansible_connection=paramiko ansible_ssh_user=$1 ansible_python_interpreter=/usr/local/bin/python" > tmphosts
+	fi
 	ansible-playbook one-time-setup.yml -ki ./tmphosts
 fi
 rm tmphosts
