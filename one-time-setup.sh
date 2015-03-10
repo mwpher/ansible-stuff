7881512e0bedaf97dead3f930d2aa7ed85b62156
Matthew Pherigo <hybrid120@gmail.com>
2015-03-09 19:15:29 -0500

    one-time-setup.sh: fix bugs, disable paramiko

--- one-time-setup.sh
+++ one-time-setup.sh
@@ -1,21 +1,20 @@
 #!/bin/sh
+set -e
 
-PYTHON_COMMAND='env ASSUME_ALWAYS_YES=YES pkg update; env ASSUME_ALWAYS_YES=YES pkg install python'
+PYTHON_COMMAND='env ASSUME_ALWAYS_YES=YES pkg update -f; env ASSUME_ALWAYS_YES=YES pkg install python'
 
 ([ $# == '2' ] || exit)
 
 if [ $1 == '-j' ]; then
-	export ASSUME_ALWAYS_YES=YES
-	sudo pkg -j $2 update;
-	sudo pkg -j $2 install python;
+	sudo jexec $2 /bin/sh -c "$PYTHON_COMMAND"
 	echo "$2 ansible_connection=jail ansible_python_interpreter=/usr/local/bin/python" > tmphosts
 	sudo ansible-playbook one-time-setup.yml -i ./tmphosts
 else
 	if [ $1 == '-s' ]; then
-		echo "$3 ansible_connection=paramiko ansible_ssh_user=$2 ansible_python_interpreter=/usr/local/bin/python" > tmphosts
+		echo "$3 ansible_ssh_user=$2" > tmphosts
 	else
 		ssh -t ${1}@${2} $PYTHON_COMMAND
-		echo "temp ansible_ssh_host=$1 ansible_connection=paramiko ansible_ssh_user=$1 ansible_python_interpreter=/usr/local/bin/python" > tmphosts
+		echo "$2 ansible_ssh_user=$1" > tmphosts
 	fi
 	ansible-playbook one-time-setup.yml -ki ./tmphosts
 fi
