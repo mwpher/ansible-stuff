42ddebc8ee157ff03e7abb37387eca6e023bd276
Matthew Pherigo <hybrid120@gmail.com>
2015-03-09 23:05:28 -0500

    one-time-setup.sh: re-set paramiko as transport provider

--- one-time-setup.sh
+++ one-time-setup.sh
@@ -7,14 +7,14 @@ PYTHON_COMMAND='env ASSUME_ALWAYS_YES=YES pkg update -f; env ASSUME_ALWAYS_YES=Y
 
 if [ $1 == '-j' ]; then
 	sudo jexec $2 /bin/sh -c "$PYTHON_COMMAND"
-	echo "$2 ansible_connection=jail ansible_python_interpreter=/usr/local/bin/python" > tmphosts
+	echo "$2 ansible_connection=jail" > tmphosts
 	sudo ansible-playbook one-time-setup.yml -i ./tmphosts
 else
 	if [ $1 == '-s' ]; then
-		echo "$3 ansible_ssh_user=$2" > tmphosts
+		echo "$3 ansible_connection=paramiko ansible_ssh_user=$2 ansible_python_interpreter=/usr/local/bin/python" > tmphosts
 	else
 		ssh -t ${1}@${2} $PYTHON_COMMAND
-		echo "$2 ansible_ssh_user=$1" > tmphosts
+		echo "$2 ansible_connection=paramiko ansible_ssh_user=$1 ansible_python_interpreter=/usr/local/bin/python" > tmphosts
 	fi
 	ansible-playbook one-time-setup.yml -ki ./tmphosts
 fi
